cbfc8efc51a0a95c3df38165931bc7cd63e8d56b
Juan Carlos Neuta <jneuta@unicauca.edu.co>
2024-10-25 09:35:18 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/WorkshopHexagonalArquitectureApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.workshop_hexagonal_arquitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopHexagonalArquitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopHexagonalArquitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/WorkshopHexagonalArquitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.workshop_hexagonal_arquitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopHexagonalArquitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
