f317312229f1ef92edd3b2d03ac5c7f17ef5aa90
Juan Carlos Neuta <jneuta@unicauca.edu.co>
2024-10-26 16:18:44 -0500

    Repair of Properties and Pom

--- src/main/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/infraestructura/output/persistencia/respositorios/EspacioRepositoryInt.java
+++ src/main/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/infraestructura/output/persistencia/respositorios/EspacioRepositoryInt.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import co.edu.unicauca.asae.workshop_hexagonal_arquitecture.infraestructura.output.persistencia.entidades.EspacioFisicoEntity;
 
 public interface EspacioRepositoryInt extends CrudRepository<EspacioFisicoEntity, Integer> {
-    @Query("SELECT count(*) FROM ProductoEntity p WHERE p.nombre = ?1")
+    @Query("SELECT count(*) FROM EspacioFisicoEntity p WHERE p.nombre = ?1")
     Integer existeEspacioPorNombre(String nombre);
 }
