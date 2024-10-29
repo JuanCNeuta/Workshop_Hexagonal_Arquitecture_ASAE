1ba730f2fe2caa327d5357367540d3df5efc2f6e
Juan Carlos Neuta <jneuta@unicauca.edu.co>
2024-10-29 17:02:03 -0500

    Querry Create FranjaHoraria Correction

--- src/main/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/dominio/casosDeUso/GestionarFranjaCUAdapter.java
+++ src/main/java/co/edu/unicauca/asae/workshop_hexagonal_arquitecture/dominio/casosDeUso/GestionarFranjaCUAdapter.java
@@ -22,11 +22,18 @@ public class GestionarFranjaCUAdapter implements GestionarFranjaCUIntPort {
     @Override
     public FranjaHoraria crear(FranjaHoraria objFranja,Integer cursoId, Integer espacioFisicoId) {
         FranjaHoraria objFranjaCreado = null;
-        
-        if (!this.objGestionarFranjaGateway.verificarOcupacion(objFranja.getDia(), objFranja.getHoraInicio(), objFranja.getHoraFin(), espacioFisicoId)) {
+
+        // Verificamos si el espacio está ocupado
+        boolean estaOcupado = this.objGestionarFranjaGateway.verificarOcupacion(
+                objFranja.getDia(), objFranja.getHoraInicio(), objFranja.getHoraFin(), espacioFisicoId);
+
+        System.out.println("¿Espacio ocupado?: " + estaOcupado);
+
+        // Si está ocupado, lanzamos el error
+        if (estaOcupado){
             this.objFormateadorResultados
-                    .retornarRespuestaErrorEntidadExiste("Error, El espacio físico\r\n" + //
-                                                "está ocupado en el di\\u00E1, hora de inicio y hora fin.");
+                    .retornarRespuestaErrorEntidadExiste("Error, El espacio físico " + //
+                                                "está ocupado en el di\u00E1, hora de inicio y hora fin.");
         } else if (!this.objGestionarFranjaGateway.verificarOcupacionDocente(objFranja.getDia(), objFranja.getHoraInicio(), objFranja.getHoraFin(), espacioFisicoId)) {
             this.objFormateadorResultados
                     .retornarRespuestaErrorEntidadExiste("Error, El docente\r\n" + //
